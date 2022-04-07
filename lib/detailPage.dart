6f6f226226a4dd814693455347cf1f4b46d0bff8
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-04-07 15:41:59 +0200

    Preparing TODO in detailPage

--- lib/detailPage.dart
+++ lib/detailPage.dart
@@ -1,57 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:firstapplicationsqyavril2022/model/utilisateur.dart';
 
-class detailPage extends StatefulWidget{
+class detailPage extends StatefulWidget {
   Utilisateur user;
   detailPage({required this.user});
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-   return detailState();
+    return detailState();
   }
-
 }
 
-class detailState extends State<detailPage>{
+class detailState extends State<detailPage> {
+  late String message;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-
-      ),
-    body : bodyPage()
-    );
+        appBar: AppBar(
+            title: Text("Chat with ${widget.user.prenom} ${widget.user.nom}")),
+        body: bodyPage());
   }
 
-
-  Widget bodyPage(){
+  Widget bodyPage() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Hero(
-            tag: widget.user.uid,
-            child: Container(
-              height : 150,
-              width: 150,
-              decoration: BoxDecoration(
-                  shape:BoxShape.circle,
-                  image: DecorationImage(
-                      image : (widget.user.logo!=null)?NetworkImage(widget.user.logo!):NetworkImage("https://firebasestorage.googleapis.com/v0/b/projetsqyavril2022.appspot.com/o/noImage.png?alt=media&token=dc26627a-5c9f-491b-8529-c8b44bfad00a")
-                  )
-              ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Taper votre message",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
+          ),
+          onChanged: (value) {
+            setState(() {
+              message = value;
+              // envoyer a la bd
+              // reset la value du textfield
+              // get la bd
+            });
+          },
         ),
-        //Image
-
-        SizedBox(height:15),
-
-
-
-        //nom et prénom
-        Text("${widget.user.prenom} ${widget.user.nom}")
+        // liste des messages éxistant
       ],
     );
   }
-
 }
